6816370859fc26574c40f836346bfcd665225a04
RobertLita <lita.robert@yahoo.com>
2023-12-09 13:29:45 +0200

    excels to df

--- dm&da/main.py
+++ dm&da/main.py
@@ -1,10 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import cv2
-from minio import Minio
-import io
-import os
-import imghdr
 from typing import Dict
 import matplotlib.pyplot as plt
 
@@ -16,15 +11,7 @@ import sys
 
 @app.get("/classify/")
 async def classify_image(image_url: str) -> str:
-    data = minio_client.get_object(bucket_name, image_url)
-    content = io.BytesIO(data.read())
-    content.seek(0)
-    img_array = read_image(content.getvalue())
-    predictions = prediction(img_array)
-    if predictions:
-        return predictions
-    else:
-        return 'empty'
+    pass
 
 if __name__ == "__main__":
     import uvicorn
