b129cde28d40d199d6dbfbaa08bb6b4c5e36ef3d
CristiGitHub <lefterioancristian@gmail.com>
2023-12-10 07:43:54 +0200

    afdasfa

--- dm&da/recomandation.py
+++ dm&da/recomandation.py
@@ -1,43 +1,39 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
 
-# Example DataFrame loading
+# Load DataFrame
 df = pd.read_csv('transactions_sales_df.csv')
 
-# Handling missing data
-df = df.dropna() # or use other strategies like filling missing values
+# Handle missing data
+df.dropna(inplace=True)
 
-# Data normalization for 'price' column
+# Normalize 'Price' column
 scaler = MinMaxScaler()
 df['Price'] = scaler.fit_transform(df[['Price']])
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 
-
-# Extracting features from descriptions
+# Extract features from 'Description'
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(df['Description'])
 
-# Clustering (for category identification)
-kmeans = KMeans(n_clusters=10) # number of clusters/categories
+# Cluster for category identification
+kmeans = KMeans(n_clusters=10)
 kmeans.fit(X)
 df['category'] = kmeans.labels_
 
-# Grouping by customer and aggregating data
+# Create user profiles by aggregating data
 user_profile = df.groupby('Customer ID').agg({
-    'category': lambda x: x.mode()[0],  # most common category
-    'Price': 'mean',  # average spending
-    # add more aggregations as needed
+    'category': lambda x: x.mode()[0],  # Most common category
+    'Price': 'mean',  # Average spending
 })
 
-print(user_profile)
-
-
-from sklearn.metrics.pairwise import cosine_similarity
-
 def recommend_products(customer_id, num_recommendations=5):
-    # Assuming 'user_profile' and 'df' from previous steps are available
+    if customer_id not in user_profile.index:
+        print(f"Customer ID {customer_id} not found.")
+        return None
+
     user_cat = user_profile.loc[customer_id, 'category']
     similar_products = df[df['category'] == user_cat]
 
@@ -45,5 +41,7 @@ def recommend_products(customer_id, num_recommendations=5):
     recommendations = similar_products.sample(n=num_recommendations)
     return recommendations['Product_Id']
 
-recommend_products('12346',3)
+# Replace '12346' with a valid customer ID from your DataFrame
+# Make sure the format (string or integer) matches your DataFrame
+recommend_products('16321', 3)
 
